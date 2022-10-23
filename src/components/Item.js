5575d590abecb361317984feab3845a1ac0e743f
Latoshia Williams <lswillms@yahoo.com>
2022-10-23 12:11:01 -0500

    commit tree e8becafc23f34cdb1875587bcf1128860b0e2ae9

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function Item({ name, category }) {
+
+  const [inCart, setInCart] = useState(false)
+  const cartStatus = inCart ? "in-cart" : ""
+  const cartButton = inCart? "add" : "remove"
+  const addCart = inCart? "Remove from Cart" : "Add to Cart"
+  
+  function addFood() {
+    const newCartArray = []
+    setInCart(newCartArray)
+  }
+
   return (
-    <li className="">
+    <li className={cartStatus}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={addFood}  className={cartButton}>{addCart}</button>
     </li>
   );
 }
